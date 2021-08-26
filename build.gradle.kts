3e92b4a2005ae407397678e9a259eadc869eae34
MrFishCakes <finlayoxby@outlook.com>
2021-08-26 13:19:19 +0100

    Fix compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,74 @@
+import io.papermc.paperweight.util.constants.*
+
 plugins {
-  java
-  id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-  id("io.papermc.paperweight.patcher") version "1.1.11"
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.1.11"
 }
 
 repositories {
-  mavenCentral()
-  maven("https://papermc.io/repo/repository/maven-public/") {
-    content { onlyForConfigurations("paperclip") }
-  }
-  maven("https://maven.quiltmc.org/repository/release/") {
-    content { onlyForConfigurations("remapper") }
-  }
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    }
 }
 
 dependencies {
-  remapper("org.quiltmc:tiny-remapper:0.4.1")
-  paperclip("io.papermc:paperclip:2.0.1")
+    remapper("org.quiltmc:tiny-remapper:0.4.3")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
+    paperclip("io.papermc:paperclip:2.0.1")
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(16))
+        }
+    }
 }
 
 subprojects {
-  apply(plugin = "java")
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(16)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
 
-  java { toolchain { languageVersion.set(JavaLanguageVersion.of(16)) } }
-
-  tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-    options.release.set(16)
-  }
-
-  repositories {
-    mavenCentral()
-    maven("https://oss.sonatype.org/content/groups/public/")
-    maven("https://papermc.io/repo/repository/maven-public/")
-    maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-    maven("https://repo.aikar.co/content/groups/aikar")
-    maven("https://repo.md-5.net/content/repositories/releases/")
-    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-  }
+    repositories {
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+    }
 }
 
 paperweight {
-  serverProject.set(project(":Crunchy-Server"))
+    serverProject.set(project(":Crunchy-Server"))
 
-  usePaperUpstream(providers.gradleProperty("paperCommit")) {
-    withPaperPatcher {
-      apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-      apiOutputDir.set(layout.projectDirectory.dir("Crunchy-API"))
+    remapRepo.set("https://maven.quiltmc.org/repository/release/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-      serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-      serverOutputDir.set(layout.projectDirectory.dir("Crunchy-Server"))
+    usePaperUpstream(providers.gradleProperty("paperCommit")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("Crunchy-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("Crunchy-Server"))
+        }
     }
-  }
 }
 
 tasks.paperclipJar {
